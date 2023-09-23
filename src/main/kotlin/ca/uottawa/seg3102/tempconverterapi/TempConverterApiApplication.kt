5802287cd3e02243e55829ec32676001b0fc2ade
SenatorDingo <abhayabhilash@duck.com>
2023-09-23 19:00:55 -0400

    Full App 2

---
+++ src/main/kotlin/ca/uottawa/seg3102/tempconverterapi/TempConverterApiApplication.kt
@@ -0,0 +1,11 @@
+package ca.uottawa.seg3102.tempconverterapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TempConverterApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TempConverterApiApplication>(*args)
+}
